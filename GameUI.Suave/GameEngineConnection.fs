1bf037f86313f893e32d6a0d67c593f1d16c819c
Frank Joppe <frankjoppe@hotmail.com>
2016-01-29 21:46:20 +0100

    Added GameUI.Suave, now with proper template

---
+++ GameUI.Suave/GameEngineConnection.fs
@@ -0,0 +1,56 @@
+﻿namespace TrafficSim
+
+open Akka
+open Akkling
+open System.IO
+open GameEngine.Common
+
+module GameEngineConnection =
+    let configuration = Configuration.parse(File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("~/akka.config")))
+
+    let ActorSystem = System.create "GameStateClient" (configuration)
+
+    let PlayerCommunicator (mailbox:Actor<obj>) =
+        let rec loop senderActor = actor {
+                let! untypeMessage = mailbox.Receive()
+                match untypeMessage with
+                | :? PlayerMessage as message ->
+                    match senderActor with
+                    |   Some(dest) -> dest <! message
+                    |   None       -> mailbox.Stash()
+                | :? PlayerMessageResponse as message ->
+                    match message with
+                    | YourId(id) ->  printfn "Your Id: %A" id
+                    | YouAreRegisterd(ref) -> 
+                        printfn "I am registered"
+                        mailbox.UnstashAll()
+                        return! (loop (Some (ActorRefs.typed(ref))))
+                    | GameStarted(color, board) -> printfn "Game started and color: %A, board: %A" color board
+                    | GameOver ->  printfn "Game Over"
+                    | PlayerMadeChoice(c,i,f) -> printfn "Player made choice: %A, %d" c i
+                    | ItIsYourTurn(possibleMoves) -> printfn "It is your turn, possible: %A" possibleMoves
+                    | BoardHasChanged(state) -> printfn "Board has changed"
+                    | NoMoves -> printfn "No possible moves"
+                    | Failed err ->  printfn "Failed: %s" err
+                    | Nothing ->  printfn "Nothing"
+                | _ -> ()
+
+                return! loop senderActor
+            }
+        loop None
+    
+
+    let CreatePlayerCommunicator() = 
+        let actor = spawn ActorSystem "playerReceiver" PlayerCommunicator
+        actor
+
+
+    // this gives a warning which you can ignore
+    let registerPlayer = select<RegisterPlayerMessage> ActorSystem "akka.tcp://GameStateServer@localhost:8080/user/RegisterPlayer"
+
+
+    let playerReceiver = CreatePlayerCommunicator()
+    let playerSender = ActorRefs.typed<PlayerMessage>(playerReceiver)
+
+    registerPlayer <! GameEngine.Common.RegisterMe(ActorRefs.untyped(playerReceiver))
+
---
+++ GameUI.Suave/VisualTest.fs
@@ -0,0 +1,49 @@
+﻿namespace TrafficSim
+
+open System
+open Akka
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
+open WebSharper.Jatek
+open GameEngine.Common
+
+open Akkling
+
+module VisualTest=
+
+    module Server=
+
+        [<Remote>]
+        let RegisterUser() = async {
+                GameEngineConnection.playerSender <! WhatIsMyId
+            }
+
+    [<JavaScript>]
+    module Client =
+        let playerGuid = Var.Create ""
+
+        let GetPlayerId() = 
+            async {
+                do! Server.RegisterUser()
+            }
+            |> Async.Start
+
+        let Main() =
+            let block = Visual.Create "App_Themes/Standard/BlueHexagon.png" 50 50 60 52
+
+            let scene = Scene.Create([block])
+
+            let allScenes = [(1, scene)] |> Map.ofList
+
+            let game = Game.Create 400 400 (fun p -> Console.Log "test") allScenes 1
+
+            GetPlayerId()
+
+            div [
+                Doc.Input [] playerGuid
+//                game.Visual
+            ]
+        
